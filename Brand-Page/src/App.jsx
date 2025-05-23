fbe0d73c66c0e65ea7e631b03ab7392303333cab
RobertLeaha <robertleaha0@gmail.com>
2025-05-23 22:55:10 +0300

    BrandPage

--- Brand-Page/src/App.jsx
+++ Brand-Page/src/App.jsx
@@ -1,23 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import Navigation from "./components/navigation.jsx";
 
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="Logo">
-          <img src="/images/brand_logo.png" alt="Logo" />
-        </div>
-
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>Login</button>
-      </nav>
+      <Navigation />
     </div>
   );
 };
