8a899d62d711770b60832130fe11045e8814a5eb
RobertLeaha <robertleaha0@gmail.com>
2025-05-28 22:56:32 +0300

    Contact Us

--- Contact-Us/src/App.jsx
+++ Contact-Us/src/App.jsx
@@ -1,8 +1,18 @@
-import Navigation from "./components/Navigation/Navigation.jsx";
 import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-  return <Navigation />;
+  return (
+    <div>
+      <Navigation />
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
+    </div>
+  );
 }
 
 export default App;
